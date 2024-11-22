10e71d5f023fe2c7f3a1fba3e221400377eff49d
JACVP325 <jacvp.2009@gmail.com>
2024-11-22 14:18:07 +0000

    Add files via upload

---
+++ estrutoras de repetição/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class TerceiroPrograma 
+{
+
+    public static void Main(String[] args)
+    {
+        //exercicio 1
+
+        //logica
+
+        for (int contador = 0; contador <= 20; contador++)
+        {
+            if (contador %1 == 0)
+            {
+                Console.WriteLine(contador);
+            }
+        }
+        for (int contador = 0; contador <= 20; contador++)
+        {
+            if (contador % 2 == 0)
+            {
+                Console.WriteLine(contador);
+            }
+        }
+    }
+}
