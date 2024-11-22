b8326804e0e3dafd536a10d4b1bd3b9045cbce5c
JACVP325 <jacvp.2009@gmail.com>
2024-11-22 12:55:06 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class PrimeiroPrograma
+{
+
+    public static void Main(String[] args) 
+    {  
+
+        // variaveis
+
+        int numero1 = 10;
+        int numero2 = 20;
+
+
+        // Lógica
+
+        int soma = numero1 + numero2;
+        int subtração = numero1 - numero2;
+        int multiplicação = numero1 * numero2;
+        int divisão = numero1 / numero2;
+
+
+        //saida
+
+        Console.WriteLine(soma);
+        Console.WriteLine(subtração);
+        Console.WriteLine(multiplicação);
+        Console.WriteLine(divisão);
+    }
+}
