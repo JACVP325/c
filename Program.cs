641aeca4733f25a798275b9a8646df3f25573aa1
JACVP325 <jacvp.2009@gmail.com>
2024-11-22 12:55:28 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,118 @@
 ﻿using System;
 
-public class PrimeiroPrograma
+public class SegundoPrograma
 {
 
-    public static void Main(String[] args) 
-    {  
-
+    public static void Main(String[] args)
+    {
         // variaveis
 
-        int numero1 = 10;
-        int numero2 = 20;
+        int numero1 = 1;
+        int numero2 = 2;
+        int numero3 = 3;
+        int numero4 = 4;
 
+        //lógica/saida
 
-        // Lógica
+        if (numero1 == numero2)
+        {
+            Console.WriteLine("o numero 1 é igual ao numero 2");
+        }
+        else
+        {
+            Console.WriteLine("o numero 1 é diferente de numero 2");
+        }
 
-        int soma = numero1 + numero2;
-        int subtração = numero1 - numero2;
-        int multiplicação = numero1 * numero2;
-        int divisão = numero1 / numero2;
+        if (numero1 == numero3)
+        {
+            Console.WriteLine("o numero 1 é igual ao numero 3");
+        }
+        else
+        {
+            Console.WriteLine("o numero 1 é diferente de numero 3");
+        }
+        if (numero1 == numero4)
+        {
+            Console.WriteLine("o numero 1 é igual ao numero 4");
+        }
+        else
+        {
+            Console.WriteLine("o numero 1 é diferente de numero 4");
+        }
+        if (numero2 == numero1)
+        {
+            Console.WriteLine("o numero 2 é igual ao numero 1");
+        }
+        else
+        {
+            Console.WriteLine("o numero 2 é diferente de numero 1");
+        }
 
+        if (numero2 == numero3)
+        {
+            Console.WriteLine("o numero 2 é igual ao numero 3");
+        }
+        else
+        {
+            Console.WriteLine("o numero 2 é diferente de numero 3");
+        }
+        if (numero2 == numero4)
+        {
+            Console.WriteLine("o numero 2 é igual ao numero 4");
+        }
+        else
+        {
+            Console.WriteLine("o numero 2 é diferente de numero 4");
+        }
+        if (numero3 == numero1)
+        {
+            Console.WriteLine("o numero 3 é igual ao numero 1");
+        }
+        else
+        {
+            Console.WriteLine("o numero 3 é diferente de numero 1");
+        }
 
-        //saida
+        if (numero3 == numero2)
+        {
+            Console.WriteLine("o numero 3 é igual ao numero 2");
+        }
+        else
+        {
+            Console.WriteLine("o numero 3 é diferente de numero 2");
+        }
+        if (numero3 == numero4)
+        {
+            Console.WriteLine("o numero 3 é igual ao numero 4");
+        }
+        else
+        {
+            Console.WriteLine("o numero 3 é diferente de numero 4");
+        }
+        if (numero4 == numero1)
+        {
+            Console.WriteLine("o numero 4 é igual ao numero 1");
+        }
+        else
+        {
+            Console.WriteLine("o numero 4 é diferente de numero 1");
+        }
 
-        Console.WriteLine(soma);
-        Console.WriteLine(subtração);
-        Console.WriteLine(multiplicação);
-        Console.WriteLine(divisão);
+        if (numero4 == numero2)
+        {
+            Console.WriteLine("o numero 4 é igual ao numero 2");
+        }
+        else
+        {
+            Console.WriteLine("o numero 4 é diferente de numero 2");
+        }
+        if (numero4 == numero3)
+        {
+            Console.WriteLine("o numero 4 é igual ao numero 3");
+        }
+        else
+        {
+            Console.WriteLine("o numero 4 é diferente de numero 3");
+        }
     }
 }
