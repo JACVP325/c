b4423afb70fd3d4d7d416f18d04ce93a66294586
JACVP325 <jacvp.2009@gmail.com>
2024-11-29 12:29:36 +0000

    Add files via upload

---
+++ troca 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+    internal class SetimoPrograma
+    {
+        static void Main(string[] args)
+        {
+            int nota = 18;
+
+            switch (nota) 
+            {
+                case int n when (n >= 18):
+                Console.WriteLine("A tua nota final é um excelete");
+                break;
+                case int n when (n >= 14):
+                Console.WriteLine("A tua nota final é um bom");
+                break;
+                case int n when (n >= 10):
+                Console.WriteLine("A tua nota final é um suficiente");
+                break;
+                case int n when (n >= 0):
+                Console.WriteLine("A tua nota final é um insuficiente");
+                break;
+                default:
+                Console.WriteLine("Nota invalida");
+                break;
+            }
+        }
+    }
