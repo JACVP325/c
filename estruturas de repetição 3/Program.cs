3998baab1deafec6fcece32cb36ba6079e860008
JACVP325 <jacvp.2009@gmail.com>
2025-01-09 15:48:31 +0000

    Add files via upload

---
+++ estruturas de repetição 3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class OitavoPrograma 
+{
+    public static void Main (string[] args) 
+    {
+
+        for (int contador = 1; contador <= 10; contador++)
+        {
+            Console.WriteLine(contador * 3);
+        }
+    }
+}
