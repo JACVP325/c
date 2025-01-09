8ad72557732d914540e865f923284893cee392d4
JACVP325 <jacvp.2009@gmail.com>
2025-01-09 14:47:32 +0000

    Add files via upload

---
+++ estruturas de repetição 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class QuartoPrograma
+{
+    public static void Main (string[] args)
+    {
+        //exercicio 2
+
+
+        //variaveis
+
+        int soma = 0;
+
+        //logica
+
+        for (int contador = 0; contador <= 50; contador++)
+        {
+            if (contador % 1 == 0)
+            {
+                soma += contador;
+            }
+        }
+        Console.WriteLine("Total nº impares = " + soma);
+        for (int contador = 0; contador <= 50; contador++)
+        {
+            if (contador % 2 == 0)
+            {
+                soma += contador;
+            }
+        }
+        Console.WriteLine("Total nº pares = " + soma);
+    }
+}
