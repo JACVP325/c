67870c58f0097607c9c6a3bdf32a22b019ba9d99
JACVP325 <jacvp.2009@gmail.com>
2025-01-09 17:18:37 +0000

    Add files via upload

---
+++ estruturas de repetição 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class NonoPrograma 
+{
+    public static void Main(string[] args)
+    {
+        int contador = 1;
+        int soma = 0;
+
+        do 
+        {
+            soma = contador + soma;
+            contador++;
+            Console.WriteLine(soma);
+            
+        }
+        while (soma < 100);
+        Console.WriteLine(contador - 1);
+    }
+}
