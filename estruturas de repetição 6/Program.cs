dc50c8116c379cdd58af38fcb0f112130fcbeb96
JACVP325 <jacvp.2009@gmail.com>
2025-01-16 15:28:29 +0000

    Add files via upload

---
+++ estruturas de repetição 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Programa11 
+{
+    public static void Main(string[] args) 
+    {
+        //dados
+
+        int contador = 1;
+
+        //logica
+
+        do
+        {
+            Console.WriteLine(contador);
+
+            contador++;
+
+        }
+        while (contador <= 5);
+
+        //saida
+
+        Console.WriteLine("Contagem comcluida parabems");
+    }
+}
