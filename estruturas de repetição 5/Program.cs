ee2b9a1e1a7ae60e9acd12a21a0c7633df14bed2
JACVP325 <jacvp.2009@gmail.com>
2025-01-16 15:14:15 +0000

    Add files via upload

---
+++ estruturas de repetição 5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class DecimoPrograma 
+{
+    public static void Main(string[] args) 
+    {
+        int contador = 0;
+
+        do 
+        {
+            Console.WriteLine(contador);
+
+            contador++;
+        }
+        while (contador <= 10);
+    }
+}
