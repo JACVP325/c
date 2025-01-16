5dd968e832efc22199e64eb8ad4e0fa052b2b75e
JACVP325 <jacvp.2009@gmail.com>
2025-01-16 16:01:57 +0000

    Add files via upload

---
+++ exemplo break/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class ExmploBreak 
+{
+    public static void Main(string[] args)
+    {
+        for(int contador = 1; contador <= 10; contador++) 
+        {
+            if (contador == 5)
+            {
+                break;
+            }
+            Console.WriteLine(contador);
+        }
+    }
+}
