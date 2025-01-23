ef12c2b3c2c816986da36f0da13ba6ff49680859
JACVP325 <jacvp.2009@gmail.com>
2025-01-23 15:21:42 +0000

    Add files via upload

---
+++ exemplo metudo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class ExemploMetudo
+{
+    public static void Main(string[] args)
+    {
+        int resultado = Soma(10, 20);
+
+        Console.WriteLine("A soma é " + resultado);
+    }
+
+    static int Soma(int a, int b) 
+    {
+        return a + b;
+    }
+    
+}
---
+++ exemplo metudo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("exemplo metudo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("exemplo metudo")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("57fbe785-cf67-40de-a428-7bff8690b2b2")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
