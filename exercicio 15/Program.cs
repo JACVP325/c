a840c97f22c32437b8a96537cfc6df92fd3ecb7b
JACVP325 <jacvp.2009@gmail.com>
2025-01-24 12:20:26 +0000

    Add files via upload

---
+++ exercicio 15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Programa13 
+{
+    public static void Main(string[] args)
+    {
+        //dados
+
+        bool resultado = ParImpar (10);
+
+        //logca
+
+        if (resultado == true)
+        {
+            Console.WriteLine("O número é par");
+        }
+        else
+        {
+            Console.WriteLine("O númer é impar");
+        }
+    }
+    public static bool ParImpar(int a) 
+    {
+        return (a % 2 == 0);
+    }
+}
