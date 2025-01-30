4e44908fd968f4f498854b9f8eb301ca7c3c5ae3
JACVP325 <jacvp.2009@gmail.com>
2025-01-30 16:16:50 +0000

    Add files via upload

---
+++ exercicio 16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Programa14 
+{
+    public static void Main(string[] args) 
+    {
+        ParImpar(10);
+    }
+    public static void ParImpar(int n1)
+    {
+        if (n1 % 2 == 0)
+        {
+            Quadrado(n1);
+        }
+        else
+        {
+            Console.WriteLine("O númere não é par por isso não é possivel" +
+                " calcular o valor ao quadrado");
+        }
+    }
+    public static void Quadrado(int n2) 
+    {
+        int resultado = n2 * n2;
+
+        Console.WriteLine("O valor " +n2 + " ao quadrado é " + resultado);
+    }
+}
---
+++ exercicio 16/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("exercicio 16")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("exercicio 16")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("d7e70369-2e67-4056-bbc1-64dd2b121ac7")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
