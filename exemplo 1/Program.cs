195fa5cdcbd9d21a22ae5852e4a33aaabf9e5f4f
JACVP325 <jacvp.2009@gmail.com>
2025-02-05 17:21:08 +0000

    Add files via upload

---
+++ exemplo 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class exemplo_1
+{
+    public static void Main(string[] args) 
+    {
+        // Arayy
+
+        int[] IdadeAlunos = { 10, 15, 17, 18 };
+
+        // percorrer array com FOR
+
+        Console.WriteLine("idade percorrida com FOR");
+
+        for (int contador = 0; contador < IdadeAlunos.Length; contador++) 
+        {
+            Console.WriteLine(IdadeAlunos[contador]);
+        }
+
+        // percorrer array com FOREACH
+
+        Console.WriteLine("Idade percorrida com FOREACH");
+
+        foreach(int idade in IdadeAlunos)
+        {
+            Console.WriteLine(idade);
+        }
+    }
+}
