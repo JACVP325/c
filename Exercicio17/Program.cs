68b3bede5ed257c52d8424e69cd73e83f4709bbb
JACVP325 <jacvp.2009@gmail.com>
2025-02-12 10:17:42 +0000

    Add files via upload

---
+++ Exercicio17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Programa15
+{
+    public static void Main(string[] args) 
+    {
+        // arrays
+
+        string[] nomefilmes = {"Harry potter", "Zorro", "Simpsons" };
+        int[] anofilmes = {1999, 2010, 2014 };
+
+        // coisas que aparecem na tela
+
+        Console.WriteLine("Lista de filmes: ");
+
+        for (int contador = 0; contador < anofilmes.Length; contador++)
+        {
+            Console.WriteLine(nomefilmes[contador] + " - " + anofilmes[contador]);
+        }
+    }
+}
